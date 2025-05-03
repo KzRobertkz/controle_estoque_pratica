0866c11ebfd7bc93ca3970acdb629abf15d74265
Robert <robert.44azz@gmail.com>
2025-05-03 17:22:50 -0300

    Add new routes and pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Header() {
+    return(
+        <div className="text-red-500 text-2xl ">Header</div>
+    )
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import Home from './pages/home.jsx'
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world grande!</div>,
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
